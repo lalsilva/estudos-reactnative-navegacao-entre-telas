701b5e6bd6b1ce6fc66ac07b92af66a5a24a0975
Luiz Silva <lasilva35@stefanini.com>
2022-07-22 17:57:22 -0300

    remoção de import desnecessário

--- src/telas/Cesta/componentes/Item.js
+++ src/telas/Cesta/componentes/Item.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Image, StyleSheet, FlatList} from 'react-native';
+import {View, Image, StyleSheet} from 'react-native';
 
 import Texto from '../../../componentes/Texto';
 
