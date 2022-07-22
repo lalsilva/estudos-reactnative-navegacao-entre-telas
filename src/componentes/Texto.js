ddfb67aadd4504c22efb1427a458b3bcdaa9a83e
Luiz Silva <lasilva35@stefanini.com>
2022-07-22 17:56:29 -0300

    ajustes relacionados ao uso do eslint

--- src/componentes/Texto.js
+++ src/componentes/Texto.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import {Text, StyleSheet} from 'react-native';
 
-export default function Texto({ children, style }) {
+export default function Texto({children, style}) {
   let estilo = estilos.texto;
-  
-  if(style?.fontWeight === 'bold') {
+
+  if (style?.fontWeight === 'bold') {
     estilo = estilos.textoNegrito;
   }
 
-  return <Text style={[style, estilo]}>{ children }</Text>
+  return <Text style={[style, estilo]}>{children}</Text>;
 }
 
 const estilos = StyleSheet.create({
@@ -19,5 +19,5 @@ const estilos = StyleSheet.create({
   textoNegrito: {
     // fontFamily: 'MontserratBold',
     fontWeight: 'bold',
-  }
+  },
 });
--- src/telas/Home/index.js
+++ src/telas/Home/index.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import { FlatList, Text, StyleSheet } from 'react-native';
+import {FlatList, Text, StyleSheet} from 'react-native';
 
 import Produtor from './componentes/Produtor';
 import Topo from './componentes/Topo';
 import useProdutores from '../../hooks/useProdutores';
 import useTextos from '../../hooks/useTextos';
 
-export default function Produtores({ melhoresProdutores }) {
+export default function Produtores({melhoresProdutores}) {
   const lista = useProdutores(melhoresProdutores);
-  const { tituloProdutores } = useTextos();
+  const {tituloProdutores} = useTextos();
 
   const TopoLista = () => {
-    return <>
-      <Topo melhoresProdutores={melhoresProdutores} />
-      <Text style={estilos.titulo}>{tituloProdutores}</Text>
-    </>
-  }
+    return (
+      <>
+        <Topo melhoresProdutores={melhoresProdutores} />
+        <Text style={estilos.titulo}>{tituloProdutores}</Text>
+      </>
+    );
+  };
 
-  return <FlatList
-    data={lista}
-    renderItem={
-      ({ item }) => <Produtor {...item} aoPressionar={() => {}} />
-    }
-    keyExtractor={({ nome }) => nome}
-    ListHeaderComponent={TopoLista}
-    style={estilos.lista} />
+  return (
+    <FlatList
+      data={lista}
+      renderItem={({item}) => <Produtor {...item} aoPressionar={() => {}} />}
+      keyExtractor={({nome}) => nome}
+      ListHeaderComponent={TopoLista}
+      style={estilos.lista}
+    />
+  );
 }
 
 const estilos = StyleSheet.create({
@@ -38,5 +41,5 @@ const estilos = StyleSheet.create({
     marginTop: 16,
     fontWeight: 'bold',
     color: '#464646',
-  }
-})
+  },
+});
